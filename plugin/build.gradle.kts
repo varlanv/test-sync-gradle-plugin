34f01125511d356e430ae33c6128a8f4d1002a1b
Vladyslav Varlan <varlanv96@gmail.com>
2024-11-10 20:12:07 +0200

    Initial version ready for publishing

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,16 +1,22 @@
 plugins {
+    id("com.gradle.plugin-publish").version("1.3.0")
     alias(libs.plugins.huskitInternalConvention)
 }
 
 gradlePlugin {
+    website = "https://github.com/varlanv/gradle-test-sync-plugin"
+    vcsUrl = "https://github.com/varlanv/gradle-test-sync-plugin.git"
     plugins {
         create("huskitTestSyncPlugin") {
             id = "com.huskit.gradle.testsync-plugin"
+            displayName = "Huskit Test Sync Plugin"
+            description = "Plugin for synchronizing parallel tests across multiple Gradle modules"
             implementationClass = "com.huskit.gradle.testsync.HuskitTestSyncPlugin"
+            tags.addAll("test", "integrationTest", "junit", "sync", "synchronization", "parallel", "multimodule")
         }
     }
 }
 
-dependencies{
+dependencies {
     compileOnly(projects.constants)
 }
